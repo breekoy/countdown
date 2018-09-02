460440f8db7d541f0e0547d7053e5d871472fc75
Bree Dingding <mcwall.breekoy@gmail.com>
2018-09-02 15:24:05 +0800

    changed API link from http to https

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$.getJSON('http://api.timezonedb.com/v2.1/get-time-zone?key=44QN6ASGVSEB&format=json&by=zone&zone=Asia/Manila', function(data){
+$.getJSON('https://api.timezonedb.com/v2.1/get-time-zone?key=44QN6ASGVSEB&format=json&by=zone&zone=Asia/Manila', function(data){
 			let dateNow = new Date(data.formatted);
 			let dateChristmas = new Date("Dec 25, 2018 00:00:00");
 			let dateNewYear = new Date("Jan 01, 2019 00:00:00");
