2314607fde7f6b654beb68347f62898614904103
Bree Dingding <coredev.breedingding@gmail.com>
2018-12-20 11:03:19 +0800

    add logic where js continues to run of following xmas/newyear

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 $.getJSON('https://api.timezonedb.com/v2.1/get-time-zone?key=44QN6ASGVSEB&format=json&by=zone&zone=Asia/Manila', function(data){
 			let dateNow = new Date(data.formatted);
-			let dateChristmas = new Date("Dec 25, 2018 00:00:00");
-			let dateNewYear = new Date("Jan 01, 2019 00:00:00");
+			let dateChristmas = new Date(dateNow.getFullYear(), 11, 25);
+			let dateNewYear = new Date(dateNow.getFullYear() + 1, 0, 1);
 			let differenceChristmas = dateChristmas.getTime() - dateNow.getTime();
 			let differenceNewYear = dateNewYear.getTime() - dateNow.getTime();
+
+			dateChristmas = (differenceChristmas <= 0) 
+				? new Date(dateChristmas.getFullYear() + 1, dateChristmas.getMonth(), dateChristmas.getDate())
+				: dateChristmas;
+
+			dateNewYear = (differenceNewYear <= 0) 
+				? new Date(dateNewYear.getFullYear() + 1, dateNewYear.getMonth(), dateChristmas.getDate())
+				: dateNewYear;
+
+			differenceChristmas = dateChristmas.getTime() - dateNow.getTime();
+			differenceNewYear = dateNewYear.getTime() - dateNow.getTime();
+
 			
 			let timer = setInterval(function(){
 				let daysLeftBeforeXmas = Math.floor(differenceChristmas / (1000 * 60 * 60 * 24));
@@ -19,10 +31,10 @@ $.getJSON('https://api.timezonedb.com/v2.1/get-time-zone?key=44QN6ASGVSEB&format
 				const pluralize = (count, noun, suffix = 's ') =>
   					`${count} ${noun}${count !== 1 ? suffix : ' '}`;
 
-				document.getElementById('countdownChristmas').innerHTML = pluralize(daysLeftBeforeXmas + 'day') + 
-					pluralize(hoursLeftBeforeXmas + 'hour') + 
-					pluralize(minutesLeftBeforeXmas + 'minute') +
-					pluralize(secondsLeftBeforeXmas + 'second') +
+				document.getElementById('countdownChristmas').innerHTML = pluralize(daysLeftBeforeXmas, 'day') + 
+					pluralize(hoursLeftBeforeXmas, 'hour') + 
+					pluralize(minutesLeftBeforeXmas, 'minute') +
+					pluralize(secondsLeftBeforeXmas, 'second') +
 					'before christmas!';
 
 				document.getElementById('countdownNewYear').innerHTML = pluralize(daysLeftBeforeNewYear, 'day') + 
