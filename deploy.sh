f67a255643b5134c9945d8393d02aa189b63ba83
breekoy <mcwall.breekoy@gmail.com>
2019-11-21 14:01:27 +0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f breekoy@github.com:breekoy/countdown.git vue:gh-pages
+git push -f git@github.com:breekoy/countdown.git master:gh-pages
 
 cd -
