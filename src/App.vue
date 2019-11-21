3b470ae0361116b691687a774beef26adebb22d3
breekoy <mcwall.breekoy@gmail.com>
2019-11-21 09:32:50 +0800

    remove static object, use Axios to fetch current time object from API

--- src/App.vue
+++ src/App.vue
@@ -28,26 +28,31 @@
     },
     methods: {
       init () {
-        // Axios.get('https://api.timezonedb.com/v2.1/get-time-zone?key=44QN6ASGVSEB&format=json&by=zone&zone=Asia/Manila')
-        //   .then(response => {
-        //     this.currentTime = response.data
+        Axios.get('https://api.timezonedb.com/v2.1/get-time-zone?key=44QN6ASGVSEB&format=json&by=zone&zone=Asia/Manila')
+          .then(response => {
+            this.currentTime = response.data
             bus.$emit('time-updated', this.currentTime)
+          })
 
-        this.currentTime = {
-          status:"OK",
-          message:"",
-          countryCode:"PH",
-          countryName:"Philippines",
-          zoneName:"Asia/Manila",
-          abbreviation:"PHT",
-          gmtOffset:28800,
-          dst:"0",
-          zoneStart:275151600,
-          zoneEnd:null,
-          nextAbbreviation:null,
-          timestamp:1574285150,
-          formatted:"2019-11-20 21:25:50"
-        }
+        // setTimeout(() => {
+        //   this.currentTime = {
+        //     status:"OK",
+        //     message:"",
+        //     countryCode:"PH",
+        //     countryName:"Philippines",
+        //     zoneName:"Asia/Manila",
+        //     abbreviation:"PHT",
+        //     gmtOffset:28800,
+        //     dst:"0",
+        //     zoneStart:275151600,
+        //     zoneEnd:null,
+        //     nextAbbreviation:null,
+        //     timestamp:1574285150,
+        //     formatted:"2019-11-20 21:25:50"
+        //   }
+
+        //   bus.$emit('time-updated', this.currentTime)
+        // }, 3000)
       }
     }
   }
